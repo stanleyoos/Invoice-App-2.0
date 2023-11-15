615c57d766461f277a216c1f04bb0674dfbf1236
stanleyos <stanislaw.sarka@gmail.com>
2023-11-15 14:10:18 +0100

    Init commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,8 @@
-export default function Home() {
+const Page = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="italic text-4xl text-sky-500">Hello Next!</h1>
+      <h1 className="italic text-4xl text-sky-500">Dashboard</h1>
     </main>
   )
 }
+export default Page
