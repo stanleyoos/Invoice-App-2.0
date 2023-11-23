d1c8522d02d4f6708e03dbeb2f118acd922555ee
stanleyos <stanislaw.sarka@gmail.com>
2023-11-23 15:06:38 +0100

    Fixed dark theme for Clerk components, addedGithub

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,20 @@
+"use client"
+
 import React from "react"
 import { SignIn } from "@clerk/nextjs"
+import { useTheme } from "next-themes"
+import { dark } from "@clerk/themes"
 
 const SignInPage = () => {
+  const { theme } = useTheme()
+
   return (
     <div className="flex justify-center">
-      <SignIn />
+      <SignIn
+        appearance={{
+          baseTheme: theme == "light" ? undefined : dark,
+        }}
+      />
     </div>
   )
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,21 @@
+"use client"
+
 import React from "react"
 import { SignUp } from "@clerk/nextjs"
+import { useTheme } from "next-themes"
+import { dark } from "@clerk/themes"
 
 const SignUpPage = () => {
+  const { theme } = useTheme()
+
   return (
     <>
       <div className="flex justify-center">
-        <SignUp />
+        <SignUp
+          appearance={{
+            baseTheme: theme == "light" ? undefined : dark,
+          }}
+        />
       </div>
     </>
   )
