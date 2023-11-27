93657b8f626905cc2ac8b60c547f4dca23a96990
stanleyos <stanislaw.sarka@gmail.com>
2023-11-27 09:19:10 +0100

    Reversed Clerk components dark theme

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,20 +1,10 @@
-"use client"
-
 import React from "react"
 import { SignIn } from "@clerk/nextjs"
-import { useTheme } from "next-themes"
-import { dark } from "@clerk/themes"
 
 const SignInPage = () => {
-  const { theme } = useTheme()
-
   return (
     <div className="flex justify-center">
-      <SignIn
-        appearance={{
-          baseTheme: theme == "light" ? undefined : dark,
-        }}
-      />
+      <SignIn />
     </div>
   )
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,21 +1,11 @@
-"use client"
-
 import React from "react"
 import { SignUp } from "@clerk/nextjs"
-import { useTheme } from "next-themes"
-import { dark } from "@clerk/themes"
 
 const SignUpPage = () => {
-  const { theme } = useTheme()
-
   return (
     <>
       <div className="flex justify-center">
-        <SignUp
-          appearance={{
-            baseTheme: theme == "light" ? undefined : dark,
-          }}
-        />
+        <SignUp />
       </div>
     </>
   )
