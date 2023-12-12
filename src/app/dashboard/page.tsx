e7ee0cf0d22e954e2d675abcda946db98eca2fc1
stanleyos <stanislaw.sarka@gmail.com>
2023-12-12 09:51:37 +0100

    Added create invoice button if any invoices

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,21 +2,28 @@ import React from "react";
 import DashboardPanel from "../../components/ui/dashboard/DashboardPanel";
 import { fetchFilteredInvoices } from "../lib/data";
 import { Invoice } from "../lib/interfaces";
-
+import CreateButton from "../../components/ui/buttons/createButton";
 
 const Page = async () => {
   const invoices: Invoice[] = await fetchFilteredInvoices("");
-  
+
   return (
     <>
       <h1 className="text-center text-6xl text-sky-500">Dashboard</h1>
-      {invoices.length == 0 ? (<>
-        <div>
-          You do not have any invoices!
-        </div>
-      </>) : (<>
-        <DashboardPanel invoices={invoices} />
-      </>)}
+      {invoices.length == 0 ? (
+        <>
+          <h1 className="text-center text-2xl  mt-12">
+            You do not have any invoices!
+          </h1>
+          <div className="flex justify-center mt-12">
+            <CreateButton href="/invoices/create" name="Add Invoice" />
+          </div>
+        </>
+      ) : (
+        <>
+          <DashboardPanel invoices={invoices} />
+        </>
+      )}
     </>
   );
 };
