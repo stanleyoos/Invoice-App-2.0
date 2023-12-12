606fea6f1d3b7de032c9326641e1c90c684c1b5b
stanleyos <stanislaw.sarka@gmail.com>
2023-12-12 09:59:24 +0100

    Added: If no customers, render add customer button

--- src/app/invoices/create/page.tsx
+++ src/app/invoices/create/page.tsx
@@ -1,13 +1,27 @@
-import React from "react"
-import Form from "../../../components/ui/invoices/create-form"
-import { fetchCustomers } from "../../lib/data"
+import React from "react";
+import Form from "../../../components/ui/invoices/create-form";
+import { fetchCustomers } from "../../lib/data";
+import CreateButton from "@/components/ui/buttons/createButton";
 
 const CreateInvoicePage = async () => {
-  const customers = await fetchCustomers()
+  const customers = await fetchCustomers();
   return (
-    <main>
-      <Form customers={customers} />
-    </main>
-  )
-}
-export default CreateInvoicePage
+    <>
+      {customers.length == 0 ? (
+        <>
+          <h1 className="text-center text-2xl  mt-12">
+            You do not have any customers!
+          </h1>
+          <div className="flex justify-center mt-12">
+            <CreateButton href="/customers/create" name="Add Customer" />
+          </div>
+        </>
+      ) : (
+        <main>
+          <Form customers={customers} />
+        </main>
+      )}
+    </>
+  );
+};
+export default CreateInvoicePage;
